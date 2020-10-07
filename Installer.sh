8e72975e9d00d5d20151b8f384179d7308b9b1db
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-10-06 21:50:41 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -9,6 +9,7 @@ apt autoremove -y
 pip3 install spleeter
 pip3 install tensorflow-gpu==1.15
 unzip "/content/teste/ni-stem/gpac.zip"
+mv "/content/teste/gpac/" "/content/teste/ni-stem/"
 chmod -R 755 "/content/teste/ni-stem/"
 chmod -R 755 "/content/teste/ni-stem/gpac/"
-rm -rf "/content/teste/ni-stem/__MACOSX/"
+rm -rf "/content/teste/__MACOSX"
