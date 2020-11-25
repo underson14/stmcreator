ff6b174ca967a632981f67bcd743cbfc307cdc68
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-11-25 00:45:47 -0300

    Update mp3.sh

--- mp3.sh
+++ mp3.sh
@@ -1,1 +1,1 @@
-youtube-dl -cio '/content/drive/My Drive/youtube-dl/MP3/%(title)s.%(ext)s' --hls-prefer-native --embed-thumbnail --add-metadata -f bestaudio --extract-audio --audio-format mp3 --audio-quality 320k $1
+youtube-dl -cio '/content/drive/MyDrive/MP3/%(title)s.%(ext)s' --hls-prefer-native --embed-thumbnail --add-metadata -f bestaudio --extract-audio --audio-format mp3 --audio-quality 320k $1
