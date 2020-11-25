6431ea2d74a291ef336f7a06cb68b7457bfe9f34
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-11-24 23:30:18 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,8 @@
 apt install ffmpeg
 apt install gpac
 apt install -y jo
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+apt-get install -y atomicparsley
 pip install tensorflow-gpu
 pip install spleeter
 cd /content/teste
@@ -11,3 +13,6 @@ chmod -R 755 "/content/teste/ni-stem/gpac/"
 chmod -R 755 "/content/teste/creator"
 rm -rf "/content/teste/__MACOSX"
 rm -rf "/content/teste/ni-stem/gpac.zip"
+chmod a+rx /usr/local/bin/youtube-dl
+chmod -R 755 "/content/teste/video.sh"
+chmod -R 755 "/content/teste/mp3.sh"
