eba5d2a443d45b5b8b5185d18ec2160291e41663
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-12-27 01:43:17 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,14 +1,8 @@
 apt install ffmpeg
 apt install gpac
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-apt-get install -y atomicparsley
 pip3 install tidal-dl --upgrade
-pip install grpcio==1.32.0
-pip install numpy==1.19.4
-pip install tensorboard==2.4.0
-pip install tensorflow-estimator==2.4.0
-pip install tensorflow==gpu-2.4.0
 pip install spleeter
+pip install tensorflow==gpu-2.3.0
 cd /content/teste
 unzip "/content/teste/ni-stem/gpac.zip"
 mv "/content/teste/gpac/" "/content/teste/ni-stem/"
@@ -17,9 +11,11 @@ chmod -R 755 "/content/teste/ni-stem/gpac/"
 chmod -R 755 "/content/teste/creator"
 rm -rf "/content/teste/__MACOSX"
 rm -rf "/content/teste/ni-stem/gpac.zip"
-chmod a+rx /usr/local/bin/youtube-dl
 chmod -R 755 "/content/teste/video.sh"
 chmod -R 755 "/content/teste/mp3.sh"
 chmod -R 755 "/content/drive/MyDrive/Videos"
 chmod -R 755 "/content/drive/MyDrive/MP3"
 apt install jo
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+apt-get install -y atomicparsley
