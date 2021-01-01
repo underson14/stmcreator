b6afc75aefd962532d1fa6246f7506d312941e46
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-01 16:45:55 -0300

    Delete mp3.sh

--- mp3.sh
+++
@@ -1,1 +0,0 @@
-youtube-dl -cio '/content/drive/MyDrive/MP3/%(title)s.%(ext)s' --hls-prefer-native --embed-thumbnail --add-metadata -f bestaudio --extract-audio --audio-format mp3 --audio-quality 320k $1
