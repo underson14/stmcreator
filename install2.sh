5cc490bb400acd03d5f7fe78d4845ab4794065e3
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-01 16:41:03 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -29,7 +29,6 @@ pip3 install tensorflow-gpu==1.15.2
 conda install -c conda-forge spleeter-gpu --yes
 pip3 install spleeter
 apt install gpac
-pip3 install tidal-dl --upgrade
 cd /content/teste
 unzip "/content/teste/ni-stem/gpac.zip"
 mv "/content/teste/gpac/" "/content/teste/ni-stem/"
@@ -38,11 +37,5 @@ chmod -R 755 "/content/teste/ni-stem/gpac/"
 chmod -R 755 "/content/teste/creator"
 rm -rf "/content/teste/__MACOSX"
 rm -rf "/content/teste/ni-stem/gpac.zip"
-chmod -R 755 "/content/teste/video.sh"
-chmod -R 755 "/content/teste/mp3.sh"
-chmod -R 755 "/content/drive/MyDrive/Videos"
-chmod -R 755 "/content/drive/MyDrive/MP3"
 apt install jo
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-chmod a+rx /usr/local/bin/youtube-dl
 apt-get install -y atomicparsley
