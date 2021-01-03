1902edb5e7dca3735e1cc60ced58b518b2fdc257
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-03 02:12:45 -0300

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,17 @@
+pip3 uninstall folium -y
+pip3 install folium==0.2.1
+pip3 uninstall imgaug -y
+pip3 install imgaug==0.2.5
+apt install ffmpeg
+pip3 install pydub
+pip3 install spleeter-gpu
+apt install -y gpac 
+cd /content/teste
+unzip "/content/teste/ni-stem/gpac.zip"
+mv "/content/teste/gpac/" "/content/teste/ni-stem/"
+chmod -R 755 "/content/teste/ni-stem/"
+chmod -R 755 "/content/teste/ni-stem/gpac/"
+chmod -R 755 "/content/teste/creator"
+rm -rf "/content/teste/__MACOSX"
+rm -rf "/content/teste/ni-stem/gpac.zip"
+apt install -y jo
