91e627f6ed13e5003805a9c331e309b6e65b74c7
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-03 03:04:10 -0300

    Update StemCreator.sh

--- StemCreator.sh
+++ StemCreator.sh
@@ -5,8 +5,8 @@ rename "s/'//g" *
 rename 's/$//g' *
 rename 's/%//g' *
 
-time for f in *.*; do
-    "/content/teste/creator" -i "$f"
+for f in *.*; do
+   time "/content/teste/creator" -i "$f"
 done
 
 rm -rf "./pretrained_models"
