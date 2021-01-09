59c0bf1e9db37aeafdcf40f8561d117bffae6a58
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-09 20:37:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ pip3 uninstall tensorflow==2.4.0 -y
 pip3 install tensorflow==2.3.0
 pip3 install spleeter-gpu
 apt install -y gpac 
-cd /content/teste
-unzip "/content/teste/ni-stem/gpac.zip"
-mv "/content/teste/gpac/" "/content/teste/ni-stem/"
-chmod -R 755 "/content/teste/ni-stem/"
-chmod -R 755 "/content/teste/ni-stem/gpac/"
-chmod -R 755 "/content/teste/creator"
-rm -rf "/content/teste/__MACOSX"
-rm -rf "/content/teste/ni-stem/gpac.zip"
+cd /content/stmcreator
+unzip "/content/stmcreator/ni-stem/gpac.zip"
+mv "/content/stmcreator/gpac/" "/content/stmcreator/ni-stem/"
+chmod -R 755 "/content/stmcreator/ni-stem/"
+chmod -R 755 "/content/tstmcreator/ni-stem/gpac/"
+chmod -R 755 "/content/stmcreator/creator"
+rm -rf "/content/stmcreator/__MACOSX"
+rm -rf "/content/stmcreator/ni-stem/gpac.zip"
 apt install -y jo
 apt install -y mediainfo
