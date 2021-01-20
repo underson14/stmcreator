690cf8cbf7edfad5d66b65c8ef41f79f6c9913d2
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-20 15:35:47 -0300

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,21 @@
+python -m pip install --upgrade pip
+pip3 uninstall folium -y
+pip3 install folium==0.2.1
+pip3 uninstall imgaug -y
+pip3 install imgaug==0.2.5
+apt install ffmpeg
+pip3 install pydub
+pip3 uninstall tensorflow==2.4.0 -y
+pip3 install tensorflow==2.3.0
+pip3 install spleeter-gpu
+apt install -y gpac 
+cd /stmcreator
+unzip "/stmcreator/ni-stem/gpac.zip"
+mv "/stmcreator/gpac/" "/stmcreator/ni-stem/"
+chmod -R 755 "/stmcreator/ni-stem/"
+chmod -R 755 "/stmcreator/ni-stem/gpac"
+chmod -R 755 "/stmcreator/creator"
+rm -rf "/stmcreator/__MACOSX"
+rm -rf "/stmcreator/ni-stem/gpac.zip"
+apt install -y jo
+apt install -y mediainfo
