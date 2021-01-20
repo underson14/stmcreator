ea4389421f21234f9faf2efc86e8a1045962d819
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-20 17:34:48 -0300

    Update install_miniconda.sh

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,4 +1,5 @@
-MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh 
+#MINICONDA_INSTALLER_SCRIPT=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_PREFIX=/usr/local
 wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
 chmod +x $MINICONDA_INSTALLER_SCRIPT
