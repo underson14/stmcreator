c8a7e43ac4ff218786c61ed46028c2def896b88a
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-01-20 16:28:37 -0300

    Update and rename install1.sh to install_miniconda.sh

---
+++ install_miniconda.sh
@@ -0,0 +1,11 @@
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh
+MINICONDA_PREFIX=/usr/local
+wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
+chmod +x $MINICONDA_INSTALLER_SCRIPT
+./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
+
+conda install --channel defaults conda python=3.6 --yes
+conda update --channel defaults --all --yes
+
+conda config --add channels conda-forge
+
