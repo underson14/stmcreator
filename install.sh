f70dd4fcac928023fd681c0f2224374a40e93d00
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-03-25 22:01:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 python -m pip install --upgrade pip
-pip3 install mutagen
 pip3 uninstall folium -y
 pip3 install folium==0.2.1
 pip3 uninstall imgaug -y
 pip3 install imgaug==0.2.5
 apt install ffmpeg
-pip3 install pydub
-pip3 uninstall tensorflow==2.4.0 -y
-pip3 install tensorflow==2.3.0
-pip3 install spleeter-gpu
+pip3 install spleeter
 apt install -y gpac 
 cd /content/stmcreator
 unzip "/content/stmcreator/ni-stem/gpac.zip"
@@ -19,4 +15,4 @@ chmod -R 755 "/content/stmcreator/creator"
 rm -rf "/content/stmcreator/__MACOSX"
 rm -rf "/content/stmcreator/ni-stem/gpac.zip"
 apt install -y jo
-apt install -y mediainfo
+
