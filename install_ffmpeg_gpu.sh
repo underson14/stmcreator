a64121b0381e21902146a9eb664e0c4579cca519
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-05-17 01:40:35 -0300

    Create install_ffmpeg_gpu.sh

---
+++ install_ffmpeg_gpu.sh
@@ -0,0 +1,19 @@
+pip3 uninstall folium -y
+pip3 install folium==0.2.1
+pip3 uninstall imgaug -y
+pip3 install imgaug==0.2.5
+git clone https://github.com/rokibulislaam/colab-ffmpeg-cuda.git
+cp -r ./colab-ffmpeg-cuda/bin/. /usr/bin/
+pip3 install spleeter
+apt install -y gpac 
+cd /content/stmcreator
+unzip "/content/stmcreator/ni-stem/gpac.zip"
+mv "/content/stmcreator/gpac/" "/content/stmcreator/ni-stem/"
+chmod -R 755 "/content/stmcreator/ni-stem/"
+chmod -R 755 "/content/stmcreator/ni-stem/gpac"
+chmod -R 755 "/content/stmcreator/creator"
+rm -rf "/content/stmcreator/__MACOSX"
+rm -rf "/content/stmcreator/ni-stem/gpac.zip"
+apt install -y jo
+apt install zip unzip
+apt autoremove
