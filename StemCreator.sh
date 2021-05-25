a6dfbae398bb7d22a6eafd9f894e9189903a444b
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-05-25 15:34:41 -0300

    Update StemCreator.sh

--- StemCreator.sh
+++ StemCreator.sh
@@ -10,12 +10,14 @@ for f in *.*; do
    time "/content/stmcreator/creator" -i "$f"
 done
 
-zip -r "stem.zip" "stems"
+data=`/bin/date +%d-%m-%Y`
+
+zip -r "stem - ${data}.zip" "stems"
 
 rm -rf "./pretrained_models"
 
-mkdir Flac
+mkdir Flac - ${data}
 
 for f in *.flac; do
-   time mv "$f" "Flac"
+   time mv "$f" "Flac-${data}"
 done
