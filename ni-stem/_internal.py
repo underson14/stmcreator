4aafb81ffd4e3ae49a1229acfbcce864646130bf
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2023-07-31 00:02:12 -0300

    Update _internal.py

--- ni-stem/_internal.py
+++ ni-stem/_internal.py
@@ -11,7 +11,7 @@ import subprocess
 import sys
 
 stemDescription  = 'stem-meta'
-stemOutExtension = ".mp4"
+stemOutExtension = ".m4a"
 
 _windows = platform.system() == "Windows"
 
@@ -90,29 +90,34 @@ class StemCreator:
             return trackPath
 
         if fileExtension in _supported_files_conversion:
-            print("\nconvertendo " + trackPath + " para " + self._format + "...")
+            print("\nconverting " + trackPath + " to " + self._format + "...")
             sys.stdout.flush()
 
-            newPath = trackName + ".mp4"
+            newPath = trackName + ".m4a"
             _removeFile(newPath)
 
             converter = os.path.join(_getProgramPath(), "avconv_win", "avconv.exe") if _windows else "ffmpeg"
             converterArgs = [converter]
-
             
-            converterArgs.extend(["-i"  , trackPath])
-            if self._format == "libfdk_aac":
-                converterArgs.extend(["-b:a", "328k"])
+            if _windows:
+                converterArgs.extend(["-i"  , trackPath])
+                if self._format == "libfdk_aac":
+                    converterArgs.extend(["-b:a", "328k"])
+                else:
+                    converterArgs.extend(["-c:a", self._format])
             else:
-                converterArgs.extend(["-c:a", self._format])
-           
+                converterArgs.extend(["-i"  , trackPath])
+                if self._format == "libfdk_aac":
+                    converterArgs.extend(["-b:a", "328k"])
+                else:
+                    converterArgs.extend(["-c:a", self._format])
 
             converterArgs.extend([newPath])
             subprocess.check_call(converterArgs)
             return newPath
         else:
-            print("formato de arquivo invalido \"" + fileExtension + "\"")
-            print("formatos validos sao " + ", ".join(_supported_files_conversion))
+            print("invalid input file format \"" + fileExtension + "\"")
+            print("valid input file formats are " + ", ".join(_supported_files_conversion))
             sys.exit()
 
     def save(self, outputFilePath = None):
@@ -131,76 +136,167 @@ class StemCreator:
         executable = "mp4box.exe" if _windows else "/usr/bin/MP4Box"
         mp4box     = os.path.join(_getProgramPath(), folderName, executable)
         
-        print("\n[Feito 0/6]\n")
+        print("\n[Done 0/6]\n")
         sys.stdout.flush()
         
         callArgs = [mp4box]
         callArgs.extend(["-add", self._convertToFormat(self._mixdownTrack, format) + "#ID=Z", outputFilePath])
-        print("\n[Feito 1/6]\n")
+        print("\n[Done 1/6]\n")
         sys.stdout.flush()
         conversionCounter = 1
         for stemTrack in self._stemTracks:
             callArgs.extend(["-add", self._convertToFormat(stemTrack, format) + "#ID=Z:disable"])
             conversionCounter += 1
-            print("\n[Feito " + str(conversionCounter) + "/6]\n")
+            print("\n[Done " + str(conversionCounter) + "/6]\n")
             sys.stdout.flush()
-        callArgs.extend(["-udta", "0:type=stem:src=base64," + base64.b64encode(json.dumps(self._metadata).encode('utf-8')).decode('utf-8')])
+
+        metadata_dict = {}
+        metadata_dict['stems'] = self._metadata
+
+        callArgs.extend(["-udta", "0:type=stem:src=base64," + base64.b64encode(json.dumps(metadata_dict).encode('utf-8')).decode('utf-8')])
+        
         subprocess.check_call(callArgs)
         sys.stdout.flush()
 
         tags = mutagen.mp4.Open(outputFilePath)
-        if ("track" in self._tags) and (len(self._tags["track"]) > 0):
-            tags["\xa9nam"] = self._tags["track"]
-        if ("artist" in self._tags) and (len(self._tags["artist"]) > 0):
+        # name
+        if ("title" in self._tags):
+            tags["\xa9nam"] = self._tags["title"]
+        # artist
+        if ("artist" in self._tags):
             tags["\xa9ART"] = self._tags["artist"]
-        if ("comment" in self._tags) and (len(self._tags["comment"]) > 0):
-            tags["\xA9cmt"] = self._tags["comment"]
-        if ("album" in self._tags) and (len(self._tags["album"]) > 0):
-            tags["\xa9alb"] = self._tags["album"]
-        if ("album_artist" in self._tags) and (len(self._tags["album_artist"]) > 0):
+        # album
+        if ("release" in self._tags):
+            tags["\xa9alb"] = self._tags["release"]
+        # album artist
+        if ("album_artist" in self._tags):
             tags["aART"] = self._tags["album_artist"]
-        if ("key" in self._tags) and (len(self._tags["key"]) > 0):
-            tags["----:com.apple.iTunes:INITIALKEY"] = mutagen.mp4.MP4FreeForm(self._tags["key"])
-        if ("copyright" in self._tags) and (len(self._tags["copyright"]) > 0):
-            tags["cprt"] = mutagen.mp4.MP4FreeForm(self._tags["copyright"]) 
-        if ("copyright" in self._tags) and (len(self._tags["copyright"]) > 0):
-            tags["----:com.apple.iTunes:CATALOGNUMBER"] = mutagen.mp4.MP4FreeForm(self._tags["copyright"])    
-        if ("mood" in self._tags) and (len(self._tags["mood"]) > 0):
-            tags["----:com.apple.iTunes:REMIXER"] = mutagen.mp4.MP4FreeForm(self._tags["mood"])
-        if ("mood" in self._tags) and (len(self._tags["mood"]) > 0):
-            tags["----:com.apple.iTunes:MOOD"] = mutagen.mp4.MP4FreeForm(self._tags["mood"])        
-        if ("lyrics" in self._tags) and (len(self._tags["lyrics"]) > 0):
-            tags["\xA9lyr"] = mutagen.mp4.MP4FreeForm(self._tags["lyrics"])
-        if ("url" in self._tags) and (len(self._tags["url"]) > 0):
-            tags["\xA9url"] = mutagen.mp4.MP4FreeForm(self._tags["url"])      
-        if ("label" in self._tags) and (len(self._tags["label"]) > 0):
-            tags["----:com.apple.iTunes:LABEL"] = mutagen.mp4.MP4FreeForm(self._tags["label"])
-        if ("genre" in self._tags) and (len(self._tags["genre"]) > 0):
+        # remixer
+        if ("remixer" in self._tags):
+            tags["----:com.apple.iTunes:REMIXER"] = mutagen.mp4.MP4FreeForm(self._tags["remixer"].encode("utf-8"))
+        # mix
+        if ("mix" in self._tags):
+            tags["----:com.apple.iTunes:MIXER"] = mutagen.mp4.MP4FreeForm(self._tags["mix"].encode("utf-8"))
+        # producer
+        if ("producer" in self._tags):
+            tags["----:com.apple.iTunes:PRODUCER"] = mutagen.mp4.MP4FreeForm(self._tags["producer"].encode("utf-8"))
+        # label
+        if ("organization" in self._tags):
+            tags["----:com.apple.iTunes:LABEL"] = mutagen.mp4.MP4FreeForm(self._tags["organization"].encode("utf-8"))
+        if ("publisher" in self._tags):
+            tags["----:com.apple.iTunes:LABEL"] = mutagen.mp4.MP4FreeForm(self._tags["publisher"].encode("utf-8"))
+        if ("label" in self._tags):
+            tags["----:com.apple.iTunes:LABEL"] = mutagen.mp4.MP4FreeForm(self._tags["label"].encode("utf-8"))
+        # genre
+        if ("genre" in self._tags):
             tags["\xa9gen"] = self._tags["genre"]
-        if ("date" in self._tags) and (len(self._tags["date"]) > 0):
-            tags["\xa9day"] = mutagen.mp4.MP4FreeForm(self._tags["date"])                                          
-        if ("releasetime" in self._tags) and (len(self._tags["releasetime"]) > 0):
-            tags["----:com.apple.iTunes:RELEASETIME"] = mutagen.mp4.MP4FreeForm(self._tags["releasetime"])
-        if ("isrc" in self._tags) and (len(self._tags["isrc"]) > 0):
-            tags["TSRC"] = mutagen.id3.TSRC(encoding=None, text=self._tags["isrc"])
-            tags["----:com.apple.iTunes:ISRC"] = mutagen.mp4.MP4FreeForm(self._tags["isrc"])
-        if ("cover" in self._tags) and (len(self._tags["cover"]) > 0):
+        if ("style" in self._tags):
+            tags["\xa9gen"] = self._tags["style"]
+        # trkn
+        if ("track" in self._tags):
+            if ("track_count" in self._tags):
+                tags["trkn"] = self._tags["track"]
+        if ("track_no" in self._tags):
+            if ("track_count" in self._tags):
+                tags["trkn"] = [(int(self._tags["track_no"]), int(self._tags["track_count"]))]
+        # catalog number
+        if ("catalog_no" in self._tags):
+            tags["----:com.apple.iTunes:CATALOGNUMBER"] = mutagen.mp4.MP4FreeForm(self._tags["catalog_no"].encode("utf-8"))
+        # date
+        if ("year" in self._tags):
+            tags["\xa9day"] = self._tags["year"]
+        if ("date" in self._tags):
+            tags["\xa9day"] = self._tags["date"]
+        # isrc
+        if ("isrc" in self._tags):
+            tags["----:com.apple.iTunes:ISRC"] = mutagen.mp4.MP4FreeForm(self._tags["isrc"].encode("utf-8"), mutagen.mp4.AtomDataType.ISRC)
+        # barcode
+        if ("upc" in self._tags):
+            tags["----:com.apple.iTunes:BARCODE"] = mutagen.mp4.MP4FreeForm(str(self._tags["upc"]).encode("utf-8"), mutagen.mp4.AtomDataType.UPC)
+        # cover
+        if ("cover" in self._tags):
             coverPath = self._tags["cover"]
-            f = urllib.urlopen(coverPath)
-            tags["covr"] = [mutagen.mp4.MP4Cover(f.read(), 
+            with open(coverPath, "rb") as f:
+                coverData = f.read()
+            tags["covr"] = [mutagen.mp4.MP4Cover(coverData,
               mutagen.mp4.MP4Cover.FORMAT_PNG if coverPath.endswith('png') else
               mutagen.mp4.MP4Cover.FORMAT_JPEG
             )]
-            f.close()  
-
+        # description (long description)
+        if ("description" in self._tags):
+            tags["ldes"] = self._tags["description"]
+        # comment
+        if ("comment" in self._tags):
+            tags["\xa9cmt"] = self._tags["comment"]
+        # bpm
+        if ("bpm" in self._tags):
+            tags["tmpo"] = [int(self._tags["bpm"])]
+        # initial key
+        if ("initialkey" in self._tags):
+            tags["----:com.apple.iTunes:initialkey"] = mutagen.mp4.MP4FreeForm(self._tags["initialkey"].encode("utf-8"))
+        # key
+        if ("key" in self._tags):
+            tags["----:com.apple.iTunes:KEY"] = mutagen.mp4.MP4FreeForm(self._tags["key"].encode("utf-8"))
+        # album
+        if ("album" in self._tags):
+            tags["\xa9alb"] = self._tags["album"]
+        # mood
+        if ("mood" in self._tags):
+            tags["----:com.apple.iTunes:MOOD"] = mutagen.mp4.MP4FreeForm(self._tags["mood"].encode("utf-8"))
+        # grouping
+        if ("grouping" in self._tags):
+            tags["\xa9grp"] = self._tags["grouping"]
+        # composer
+        if ("composer" in self._tags):
+            tags["\xa9wrt"] = self._tags["composer"]
+        # barcode
+        if ("barcode" in self._tags):
+            tags["----:com.apple.iTunes:BARCODE"] = mutagen.mp4.MP4FreeForm(str(self._tags["barcode"]).encode("utf-8"), mutagen.mp4.AtomDataType.UPC)
+        # lyrics
+        if ("lyrics" in self._tags):
+            tags["\xa9lyr"] = self._tags["lyrics"]
+        # copyright
+        if ("copyright" in self._tags):
+            tags["cprt"] = self._tags["copyright"]
+        # url_discogs_artist_site
+        if ("url_discogs_artist_site" in self._tags):
+            tags["----:com.apple.iTunes:URL_DISCOGS_ARTIST_SITE"] = mutagen.mp4.MP4FreeForm(self._tags["url_discogs_artist_site"].encode("utf-8"))
+        # url_discogs_release_site
+        if ("www" in self._tags):
+            tags["----:com.apple.iTunes:URL_DISCOGS_RELEASE_SITE"] = mutagen.mp4.MP4FreeForm(self._tags["www"].encode("utf-8"))
+        if ("url_discogs_release_site" in self._tags):
+            tags["----:com.apple.iTunes:URL_DISCOGS_RELEASE_SITE"] = mutagen.mp4.MP4FreeForm(self._tags["url_discogs_release_site"].encode("utf-8"))
+        # youtube_id
+        if ("youtube_id" in self._tags):
+            tags["----:com.apple.iTunes:YouTube Id"] = mutagen.mp4.MP4FreeForm(self._tags["youtube_id"].encode("utf-8"))
+        # beatport_id
+        if ("beatport_id" in self._tags):
+            tags["----:com.apple.iTunes:Beatport Id"] = mutagen.mp4.MP4FreeForm(self._tags["beatport_id"].encode("utf-8"))
+        # qobuz_id
+        if ("qobuz_id" in self._tags):
+            tags["----:com.apple.iTunes:Qobuz Id"] = mutagen.mp4.MP4FreeForm(self._tags["qobuz_id"].encode("utf-8"))
+        # discogs_id
+        if ("discogs_release_id" in self._tags):
+            tags["----:com.apple.iTunes:Discogs Id"] = mutagen.mp4.MP4FreeForm(self._tags["discogs_release_id"].encode("utf-8"))
+        # media
+        if ("media" in self._tags):
+            tags["----:com.apple.iTunes:MEDIA"] = mutagen.mp4.MP4FreeForm(self._tags["media"].encode("utf-8"))
+        # country
+        if ("country" in self._tags):
+            tags["----:com.apple.iTunes:COUNTRY"] = mutagen.mp4.MP4FreeForm(self._tags["country"].encode("utf-8"))
 
         tags["TAUT"] = "STEM"
         tags.save(outputFilePath)
         
-        print("\n[Feito 6/6]\n")
+        print("\n[Done 6/6]\n")
         sys.stdout.flush()
 
-        print("A criacao de " + outputFilePath + " foi um sucesso!")
+        print("creating " + outputFilePath + " was successful!")
+        
+        print("\n[Done 6/6]\n")
+        sys.stdout.flush()
+
+        print("creating " + outputFilePath + " was successful!")
 
 
 class StemMetadataViewer:
@@ -244,5 +340,3 @@ class StemMetadataViewer:
                 raise e
             finally:
                 fileObj.close()
-
-
